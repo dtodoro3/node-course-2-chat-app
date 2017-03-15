3578d777b09155a1b0bb0a6e376de492236e20b6
Darina Todorova <dari.t.todorova@gmail.com>
2017-03-15 13:18:11 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yup, thats work for me'
-  });
 });
 
 socket.on('disconnect', function () {
